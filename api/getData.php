d4dd5975144a850d2c1b40853567c1a7a25be8de
Leo <54992384+frankeu@users.noreply.github.com>
2020-11-01 20:43:01 +0700

    Update getData.php

--- api/getData.php
+++ api/getData.php
@@ -18,7 +18,7 @@
 		"network" => array_map('intval', explode(" ",exec("cat /proc/net/dev | grep '".exec("route | grep '^default' | grep -o '[^ ]*$'").":' | awk {'print $2\" \"$3\" \"$10\" \"$11'}"))),
 		"uptime" => (int)exec("cut -d. -f1 /proc/uptime"),
 		"OS" => exec("cat /etc/*-release | grep 'PRETTY_NAME' | cut -d \\\" -f2"),
-		"izul" => exec("free | grep 'Mem:' | awk {'print $2\" \"$3\" \"$4\" \"$6'}")
+		"izul" => exec("free")
 	);
 
 	exec("cat /proc/cpuinfo | grep -i 'model name\|cpu cores\|cpu mhz'", $tmp);
